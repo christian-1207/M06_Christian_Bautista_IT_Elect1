b717412c59bbb802dda025875ff182e60a220dcb
Christian Bautista <yashians120704@gmail.com>
2025-09-20 08:53:37 +0800

    MidtermAct1

---
+++ App.js
@@ -0,0 +1,15 @@
+import ChatScreen from './ChatScreen';
+import React from "react";
+
+import CommentSection from "./CommentSection";
+
+export default function App() {
+  return  (
+   <>
+<ChatScreen />
+<CommentSection />
+
+</>
+)
+  
+}
